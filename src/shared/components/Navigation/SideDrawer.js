3cf965a8e1394bbedb603d11592c57d13bb0c967
Dariusz Franczak <darius32@op.pl>
2021-02-15 11:27:51 +0000

    navigation completed

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import ReactDom from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
-import './SideDrawer.css';
+import "./SideDrawer.css";
 
-const SideDrawer = props => {
-  return <aside className="side-drawer">{props.children}</aside>
+const SideDrawer = (props) => {
+  const { show } = props;
+
+  const content = (
+    <CSSTransition
+      in={show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer">{props.children}</aside>
+    </CSSTransition>
+  );
+
+  return ReactDom.createPortal(content, document.getElementById("side-drawer"));
 };
 
 export default SideDrawer;
